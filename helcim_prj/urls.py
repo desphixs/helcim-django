c267af73e24b6d7a4a5b3c41d479f8af73e07caf
Destiny Franks <desphixs@gmail.com>
2023-03-20 12:22:56 +0100

    First Commit

---
+++ helcim_prj/urls.py
@@ -0,0 +1,43 @@
+"""helcim_prj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, reverse_lazy
+from django.views.generic import RedirectView
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('helcim/', include('helcim.urls')),
+    path('example/', include('example_app.urls', namespace='example')),
+    path('', RedirectView.as_view(permanent=False, url=reverse_lazy('example:home')),),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
+    # import debug_toolbar
+    # urlpatterns = [
+    #     url(r'^__debug__/', include(debug_toolbar.urls)),
+    # ] + urlpatterns
